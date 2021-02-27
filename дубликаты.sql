d40099f610101718821f13202d305966c921e7ca
KnivesTix <joola1992@gmail.com>
2021-02-27 14:07:47 +0300

    Add new file

--- дубликаты.sql
+++ дубликаты.sql
@@ -1,5 +1,27 @@
+1) Просто список повторяющихся
 with c as (
 SELECT u.*, RANK() OVER (ORDER BY email) r
 FROM inet.users u
 ) 
 SELECT email, R FROM c GROUP BY email,R HAVING COUNT(*)>1
+
+2)
+select * from inet.users h 
+where exists 
+(select id from inet.users
+where email = h.email 
+and ID < h.ID)
+
+3)
+select * from inet.users t
+where id in (select id from (
+	select id, row_number() over (
+		partition by email order by email) rn 
+	from inet.users) where rn > 1)
+
+4)
+select * from inet.users i
+where id not in (
+	select min(id) id 
+	from inet.users 
+	group by email)
